15ec1f17080dba97c3b635d1b86445a596c99214
mcraet <mcraeteisha@gmail.com>
2022-02-07 00:46:33 -0500

    installed vue and other dependencies

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use app\Models\Note;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class NoteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Note::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+        ];
+    }
+}
